9d76a574b3b09840a7ee2ddedc90d01188b07f9d
George Ansong <Georgeansong7@gmail.com>
2021-09-06 00:05:30 +0000

    first iteration of communications feature

--- lib/Screens/CommScreen.dart
+++ lib/Screens/CommScreen.dart
@@ -1,10 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:dash_chat/dash_chat.dart';
+import 'package:notitia/Providers/ProjectProvider.dart';
+import 'package:notitia/Screens/VideoCallPage.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:provider/provider.dart';
 
-class CommScreen extends StatelessWidget {
+class CommScreen extends StatefulWidget {
   const CommScreen({Key? key}) : super(key: key);
 
   @override
+  _CommScreenState createState() => _CommScreenState();
+}
+
+class _CommScreenState extends State<CommScreen> {
+  Future<void> _handleCameraAndMic(Permission permission) async {
+    final status = await permission.request();
+    print(status);
+  }
+
+  Future<void> onJoin() async {
+    print("yyyyyyyyyyyyyyyyy");
+    await _handleCameraAndMic(Permission.camera);
+    await _handleCameraAndMic(Permission.microphone);
+
+    // final String chanell = Provider.of<ProjectProvider>(context).ge
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => VideoCallPage(
+            appID: "c7e1ccdaafeb4ff886c9653d018e50d3",
+            channelName: "aadsdasd",
+          ),
+        ));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(child: Text("Comms"));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Communications"),
+      ),
+      body: DashChat(
+          trailing: [
+            GestureDetector(
+              onTap: () => onJoin(),
+              child: Icon(Icons.video_call_outlined),
+            )
+          ],
+          onSend: (_) {},
+          messages: [
+            ChatMessage(
+              text: "Hello",
+              user: ChatUser(
+                name: "Jhon Doe",
+                uid: "xxxxxxxxx",
+              ),
+              createdAt: DateTime.now(),
+            )
+          ],
+          user: ChatUser(
+            name: "Jhon Doe",
+            uid: "xxxxxxxxx",
+          )),
+    );
   }
 }
---
+++ lib/Screens/VideoCallPage.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:agora_rtc_engine/rtc_engine.dart';
+import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
+import 'package:agora_rtc_engine/rtc_remote_view.dart' as RtcRemoteView;
+
+class VideoCallPage extends StatefulWidget {
+  final String appID;
+  final String channelName;
+  VideoCallPage({required this.appID, required this.channelName});
+
+  @override
+  _VideoCallPageState createState() => _VideoCallPageState();
+}
+
+class _VideoCallPageState extends State<VideoCallPage> {
+  static final _users = <int>[];
+  final _infoStrings = <String>[];
+  bool muted = false;
+  late RtcEngine _engine;
+
+  Future<void> _initAgoraRtcEngine() async {
+    _engine = await RtcEngine.create(widget.appID);
+    await _engine.enableVideo();
+  }
+
+  void _addAgoraEventHandlers() {
+    _engine.setEventHandler(RtcEngineEventHandler(
+      error: (code) {
+        setState(() {
+          final info = 'onError: $code';
+          _infoStrings.add(info);
+        });
+      },
+      joinChannelSuccess: (channel, uid, elapsed) {
+        setState(() {
+          final info = 'onJoinChannel: $channel, uid: $uid';
+          _infoStrings.add(info);
+        });
+      },
+      leaveChannel: (stats) {
+        setState(() {
+          _infoStrings.add('onLeaveChannel');
+          _users.clear();
+        });
+      },
+      userJoined: (uid, elapsed) {
+        setState(() {
+          final info = 'userJoined: $uid';
+          _infoStrings.add(info);
+          _users.add(uid);
+        });
+      },
+      userOffline: (uid, reason) {
+        setState(() {
+          final info = 'userOffline: $uid , reason: $reason';
+          _infoStrings.add(info);
+          _users.remove(uid);
+        });
+      },
+      firstRemoteVideoFrame: (uid, width, height, elapsed) {
+        setState(() {
+          final info = 'firstRemoteVideoFrame: $uid';
+          _infoStrings.add(info);
+        });
+      },
+    ));
+  }
+
+  Future<void> initialize() async {
+    if (widget.appID.isEmpty) {
+      setState(() {
+        _infoStrings.add(
+          'APP_ID missing, please provide your APP_ID in settings.dart',
+        );
+        _infoStrings.add('Agora Engine is not starting');
+      });
+      return;
+    }
+    await _initAgoraRtcEngine();
+    _addAgoraEventHandlers();
+    await _engine.joinChannel(null, widget.channelName, null, 0);
+  }
+
+  @override
+  void dispose() {
+    // clear users
+    _users.clear();
+    // destroy sdk
+    _engine.leaveChannel();
+    _engine.destroy();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // initialize agora sdk
+    initialize();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
